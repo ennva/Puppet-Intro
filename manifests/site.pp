15179dadfd6dfaa5d63f2ee61e95011dc2c00624
Ennvaweb <35902574+ennva@users.noreply.github.com>
2022-04-19 11:28:25 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,15 @@
 node default {
   # This is how you defined a resource of type file
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme file..',
-    owner   => 'root'
-  }
+  #file {'/root/README':
+  #  ensure  => file,
+  # content => 'This is a readme file..',
+  #  owner   => 'root'
+  #}
   # This will generate a duplicate error has the resource is already defined with the same owner
   # file { '/root/README':
   #  owner => 'root',
   #}
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
