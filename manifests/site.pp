6c93e6b5bd42c622af1250d1d5fbe3851639757b
Ennvaweb <35902574+ennva@users.noreply.github.com>
2022-04-19 10:35:11 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,12 @@
 node default {
+  # This is how you defined a resource of type file
   file {'/root/README':
     ensure  => file,
     content => 'This is a readme',
     owner   => 'root'
   }
-  file { '/root/README':
-    owner => 'root',
-  }
+  # This will generate a duplicate error has the resource is already defined with the same owner
+  # file { '/root/README':
+  #  owner => 'root',
+  #}
 }
